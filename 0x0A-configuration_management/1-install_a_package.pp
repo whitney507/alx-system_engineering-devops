0f31c2b3b73e8a846809b3b3158757c69a4c4606
whitney507 <whitneymarian507@gmail.com>
2024-01-19 01:38:51 -0800

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,30 @@
-# install puppet-lint -v 2.1.0
+# Using Puppet, install flask from pip3.
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
+# Requirements:
+
+# Install flask
+# Version must be 2.1.0
+
+package {
+    'python3.8':
+    ensure  => '3.8.10',
+}
+
+package {
+    'python3-pip':
+    ensure  => installed,
+}
+
+package {
+    'flask':
+    ensure   => '2.1.0',
+    require  => Package['python3-pip'],
+    provider => 'pip',
+}
+
+package {
+    'Werkzeug':
+    ensure   => '2.1.0',
+    require  => Package['python3-pip'],
+    provider => 'pip',
 }
